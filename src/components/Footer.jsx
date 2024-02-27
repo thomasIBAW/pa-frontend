4e2ff12beb04f67b48677459fe718fb6fd405474
Thomas <tbe@pm.me>
2024-02-27 15:51:07 +0100

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Box, Center} from "@chakra-ui/react";
 import Cookies from "universal-cookie";
+import UserContext from "../hooks/Contect.jsx";
 function Footer(props) {
     const bgColor='#e6c997';
     const cookies = new Cookies()
-    const user = cookies.get("currentUser")
+
+    const {currentUser} = useContext(UserContext)
 
     return (
  //TODO Remove Footer or place to the bottom of the page
     <Center display="flex" bg={bgColor} w='100%' p={1} color='black' alignItems="center" justifyContent="space-evenly" fontFamily='Julius Sans One' fontSize='smaller'>
-           <Box>User : {user}</Box>
+           <Box>User : {currentUser.username}</Box>
           <Box>|</Box>
-        <Box>Family : {user}</Box>
+        <Box>Family : {currentUser.linkedFamily}</Box>
     </Center>
 
     );
