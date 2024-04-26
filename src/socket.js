724bad6b1ebdd29ebf9079cfd50ae35d1b72877c
Thomas <tbe@pm.me>
2024-04-26 23:51:56 +0200

    corrected some URL isssue in prod related to sockets

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.DEVSTATE === 'PROD' ? "/app/" : 'http://localhost:3005';
+const URL = import.meta.env.MODE === 'production' ? '/app/' : 'http://localhost:3005';
 
 export const socket = io(URL, {});
