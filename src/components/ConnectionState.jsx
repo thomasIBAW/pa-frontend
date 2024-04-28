260cbfcdac69fa678bfca2ababf90c7ffb923660
Thomas <tbe@pm.me>
2024-04-28 17:48:39 +0200

    modified socket addr for prod*

--- src/components/ConnectionState.jsx
+++ src/components/ConnectionState.jsx
@@ -14,7 +14,7 @@ export function ConnectionState({ isConnected }) {
         setLastChange(arg)
     });
 
-    const URL = import.meta.env.MODE === 'production' ? '/app/' : 'http://localhost:3005';
+    const URL = import.meta.env.MODE === 'production' ? 'http://app.famcal.ch/ws' : 'http://localhost:3005';
 
     return (
 <>
