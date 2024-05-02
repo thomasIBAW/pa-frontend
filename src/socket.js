6ca745c3159760a9ccdd2e7fd38fcf9ec1c40844
Thomas <tbe@pm.me>
2024-05-02 12:53:05 +0200

    fixed socket.io issue

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,7 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.MODE === 'production' ? '/socket/' : 'http://localhost:3005';
+const URL = import.meta.env.MODE === "development" ? 'http://localhost:3005' : '/';
 
-export const socket = io(URL, {
-        cors: {
-            methods: ["GET", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
-            credentials: true
-}
-});
+export const socket = io(URL, {});
 
