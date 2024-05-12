800c3b94718755b99d0838ed996921a2c8518432
Thomas <tbe@pm.me>
2024-05-12 09:01:00 +0200

    modified MePage to use /me endpoint

--- src/pages/MePage.jsx
+++ src/pages/MePage.jsx
@@ -5,8 +5,11 @@
 import moment from "moment";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 import {useCookies} from "react-cookie";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Box} from "@chakra-ui/react";
 
+const devState = import.meta.env.VITE_DEVSTATE
+const backendURI = devState==='PROD' ? '/app' : 'http://localhost:3005';
 function MePage() {
     // const {currentUser} = useContext(UserContext)
     //
@@ -14,25 +17,52 @@ function MePage() {
     // const apiKey = cookies.get("jwt_auth")
     // const decodedUser = jwtDecode(apiKey)
     const [cookie] = useCookies()
-    const [user , setUser] = useState(cookie.fc_user)
+    const [user , setUser] = useState({})
+    const [cookieUser , setCookieUser] = useState(cookie.fc_user)
+
+    useEffect( () => {
+        async function getMe(){
+            const response = await fetch(`${backendURI}/me`, {
+                method: "GET", // *GET, POST, PUT, DELETE, etc.
+                mode: "cors", // no-cors, *cors, same-origin
+                // // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "include", // include, *same-origin, omit
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            if (response.status !== 200) {
+                // setError("incorrect")
+                console.log(response.status)
+                return
+            }
+            const res = await response.json();
+            setUser(res)
+        }
+        getMe()
+    }, [])
+
 
     // const auth = useAuthUser()
 
     return (
         <center>
         <h1>This is me</h1>
-            <h2>From the Cookie:</h2>
+            <h2>Checked using jwt:</h2>
+            <Box h="10px"></Box>
+
             <p><abbr title="Currently Logged In User">Username</abbr> : {user.username}</p>
             <p>linked Person : {user.linkedPerson}</p>
             <p>Linked Family : {user.linkedFamily}</p>
             <p>Server Admin : {user.isAdmin ? "Yes": "NO"}</p>
             <p>Family Admin : {user.isFamilyAdmin ? "Yes": "NO"}</p>
-            <p>User Uuid : {user.uuid}</p>
+            <p>User Uuid : {user.userUuid}</p>
             <p>Registered : {user.created}</p>
 
-            <p></p>
-            <h1>Cookie</h1>
-            <p>{JSON.stringify(user)}</p>
+            <Box h="20px"></Box>
+            <h2>Checked using the fc_user Cookie:</h2>
+            <Box h="10px"></Box>
+            <p>{JSON.stringify(cookieUser)}</p>
 
         </center>
 
